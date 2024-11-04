0668d30d67eac066a371b751c52cbded91a85826
leonardo.cigula@gmail.com <109247428+leoCigula@users.noreply.github.com>
2024-11-04 15:16:12 +0100

    Inicijalizirao spring projekt sa navedenim tehnologijama #7

---
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/gym_fitness_tracker/GymFitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.gym_fitness_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymFitnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymFitnessTrackerApplication.class, args);
+	}
+
+}
---
+++ IzvorniKod/backend/src/test/java/hr/fer/progi/gym_fitness_tracker/GymFitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.gym_fitness_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymFitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
