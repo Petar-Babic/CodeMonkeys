6cbf0646438f524669f6a8a047215697060fe223
kordicluka <luka.kordic.zg@gmail.com>
2025-01-16 18:39:23 +0100

    Dodana konfiguracija za učitavanje .env datoteke

---
+++ IzvorniKod/backend/src/main/java/GymFitnessTrackerApplication/configuration/EnvConfig.java
@@ -0,0 +1,9 @@
+package GymFitnessTrackerApplication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:.env")
+public class EnvConfig {
+} 
